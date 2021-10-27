9b389cd2a30f9d54cb5b6831ee921785966f8228
michal-jewczuk <michal.jewczuk@outlook.com>
2021-10-27 11:19:21 +0200

    Create problem list_two_thirds

---
+++ list_two_thirds.py
@@ -0,0 +1,15 @@
+"""
+    Create a method called filterGreaterThanTwoThirds that will consume a list of numbers
+    and return a sorted (in ascending order) list of numbers that are greater than 2/3 of sum of all items in original list.
+
+    When supplied something other than an empty list or a list of numbers the method should return -1.
+
+    Examples:
+    [3,2,1] produces []
+    [7,1,1,0] produces [7]
+    [-5,4,3,1] produces [3,4]
+    [] produces []
+    ['aaa', 'bbb'] produces -1
+    'invalid' produces -1 
+    true produces -1
+"""
